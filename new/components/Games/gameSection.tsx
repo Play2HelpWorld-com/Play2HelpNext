2d711c3ca746565c0742fdcf57575cab8139071c
moyasiginko <mrsiyam61@gmail.com>
2024-11-27 17:15:11 +0600

    fix the color issue

--- new/components/Games/gameSection.tsx
+++ new/components/Games/gameSection.tsx
@@ -10,14 +10,14 @@ export default function GameSection() {
           {/* Section header */}
           <div className="mx-auto max-w-3xl pb-12 text-center md:pb-20">
             <div className="inline-flex items-center gap-3 pb-3 before:h-px before:w-8 before:bg-gradient-to-r before:from-transparent before:to-indigo-200/50 after:h-px after:w-8 after:bg-gradient-to-l after:from-transparent after:to-indigo-200/50">
-              <span className="inline-flex bg-gradient-to-r from-indigo-500 to-indigo-200 bg-clip-text text-transparent">
+              <span className="inline-flex bg-gray-600 from-indigo-500 to-indigo-200 bg-clip-text text-transparent dark:bg-gradient-to-r">
                 Tailored games
               </span>
             </div>
-            <h2 className="font-nacelle animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-4 text-3xl font-semibold text-transparent md:text-4xl">
+            <h2 className="font-nacelle animate-[gradient_6s_linear_infinite] bg-gray-500 bg-[length:200%_auto] bg-clip-text pb-4 text-3xl font-semibold text-transparent dark:bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] md:text-4xl">
               Play our games
             </h2>
-            <p className="text-lg text-indigo-200/65">
+            <p className="text-lg text-gray-400 dark:text-indigo-200/65">
               Play games to donate to charity and help the world. Play, Win,
               Earn Points and Get Rewards. We have a wide range of games to
               choose from. Play now! 🎮
