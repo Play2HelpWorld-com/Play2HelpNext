459ceb0e57b8a4b913d6a611806dd36ce64566ee
moyasiginko <mrsiyam61@gmail.com>
2024-12-09 12:16:04 +0600

    add global custom preloader

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React from "react";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Lines from "@/components/Lines";
@@ -7,11 +8,17 @@ import ScrollToTop from "@/components/ScrollToTop";
 import { ThemeProvider } from "next-themes";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import ToasterContext from "./utils/context/ToastContext";
-import StoreProvider from "./utils/provider/StoreProvider";
+import ToasterContext from "@/app/utils/context/ToastContext";
+import StoreProvider from "@/app/utils/provider/StoreProvider";
 import { useEffect } from "react";
 import { useAppDispatch } from "@/configs/redux/hooks";
 import { fetchLoggedInUser } from "@/configs/redux/auth/authSlice";
+import {
+  LoadingProvider,
+  useLoading,
+} from "@/app/utils/context/LoadingContext"; // Import LoadingProvider
+import Loader from "@/components/Loader/Preloader";
+import RouteChangeHandler from "@/utils/loader/RouteChangeHandler";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,7 +30,6 @@ const ReduxInitializer = ({ children }: { children: React.ReactNode }) => {
   return <>{children}</>;
 };
 
-
 export default function RootLayout({
   children,
 }: {
@@ -32,19 +38,21 @@ export default function RootLayout({
   return (
     <StoreProvider>
       <html lang="en" suppressHydrationWarning>
-        <body className={` ${inter.className}`}>
+        <body className={`${inter.className}`}>
           <ThemeProvider
             enableSystem={false}
             attribute="class"
             defaultTheme="light"
           >
             <ReduxInitializer>
-            <Lines />
-            <Header />
-            <ToasterContext />
-            {children}
-            <Footer />
-            <ScrollToTop />
+              <Lines />
+              <Header />
+              <ToasterContext />
+              <LoadingProvider>
+                <LayoutWithLoader>{children}</LayoutWithLoader>
+              </LoadingProvider>
+              <Footer />
+              <ScrollToTop />
             </ReduxInitializer>
           </ThemeProvider>
         </body>
@@ -52,3 +60,18 @@ export default function RootLayout({
     </StoreProvider>
   );
 }
+
+// A separate component that uses the Loading context
+const LayoutWithLoader = ({ children }: { children: React.ReactNode }) => {
+  const { loading } = useLoading(); // Use the hook inside the provider
+  return (
+    <>
+      <RouteChangeHandler />
+      <Loader
+        isLoading={loading}
+        onTransitionEnd={() => console.log("Transition ended")}
+      />
+      {children}
+    </>
+  );
+};
