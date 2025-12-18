f167378acc8e110e7613104d649d9c5d3ed38cde
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2025-12-18 20:44:26 +0600

    fix lintint error

--- components/Pricing/index.tsx
+++ components/Pricing/index.tsx
@@ -235,32 +235,6 @@ const Pricing = () => {
                     d="M10.4767 6.16701L6.00668 1.69701L7.18501 0.518677L13.6667 7.00034L7.18501 13.482L6.00668 12.3037L10.4767 7.83368H0.333344V6.16701H10.4767Z"
                     fill="currentColor"
                   />
-                  <li className="mb-4 text-black last:mb-0 dark:text-manatee">
-                    Exclusive Support
-                  </li>
-                  <li className="mb-4 text-black last:mb-0 dark:text-manatee">
-                    Custom Branding Strategy
-                  </li>
-                </ul>
-              </div>
-
-              <button
-                aria-label="Get the Plan button"
-                className="group/btn inline-flex items-center gap-2.5 font-medium text-primary transition-all duration-300 dark:text-white dark:hover:text-primary"
-              >
-                <span className="duration-300 group-hover/btn:pr-2">
-                  Get the Plan
-                </span>
-                <svg
-                  width="14"
-                  height="14"
-                  viewBox="0 0 14 14"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M10.4767 6.16701L6.00668 1.69701L7.18501 0.518677L13.6667 7.00034L7.18501 13.482L6.00668 12.3037L10.4767 7.83368H0.333344V6.16701H10.4767Z"
-                    fill="currentColor"
-                  />
                 </svg>
               </button>
             </div>
@@ -273,3 +247,4 @@ const Pricing = () => {
 };
 
 export default Pricing;
+
